84aa9b6a6bb8a7362bf19309fb68aa33ce3a7804
sebastianschnock <sebastian.schnock@gmail.com>
2016-01-28 13:41:06 +0100

    Add more tests and first implementation of Shunting-yard algorithm

--- src/js/shunting-yard.js
+++ src/js/shunting-yard.js
@@ -1,12 +1,77 @@
-/**
- * Converts a string in infix notation to Reverse Polish notation (RPN).
- * eg: "1 + 2" would be converted to "1 2 +"
- * 
- * @method     convertInfixToRPN
- * @param      {String}  infix   { the string in infix to convert }
- */
-function convertInfixToRPN(infix) {
-	return '';
+const operators = [
+	{
+		symbol: '+',
+		precedence: 2,
+		leftAssociative: true
+	},
+	{
+		symbol: '-',
+		precedence: 2,
+		leftAssociative: true
+	},
+	{
+		symbol: '/',
+		precedence: 3,
+		leftAssociative: true
+	},
+	{
+		symbol: '*',
+		precedence: 3,
+		leftAssociative: true
+	},
+	{
+		symbol: '^',
+		precedence: 4,
+		leftAssociative: false
+	}
+];
+
+function getOperator(symbol) {
+	for(let operator of operators) {
+		if(operator.symbol === symbol) return operator;
+	}
+	return undefined;
 }
 
-module.exports = { convertInfixToRPN };
+/**
+ * Converts a string in infix notation to postfix oder Reverse Polish notation.
+ * eg: "1 + 2" would be converted to "1 2 +"
+ * This function uses the "Shunting-yard algorithm".
+ * 
+ * @method     convertInfixToPostfix
+ * @param      {String}  infix   the string in infix to convert
+ */
+function convertInfixToPostfix(infix) {
+
+	let outputQueue = [];
+	let operatorStack = [];
+	let operator;
+
+	let tokens = infix.split(' ');
+	for(let token of tokens) {
+
+		// add numbers to output queue
+		if(Number.isInteger(parseInt(token))) {
+			outputQueue.push(token);
+		}
+
+		else if((operator = getOperator(token)) !== undefined) {
+			while(operatorStack.length > 1 &&
+				((operator.leftAssociative && operator.precedence <= operatorStack[0].precedence) ||
+				(!operator.leftAssociative && operator.precedence < operatorStack[0]))) {
+				outputQueue.push(operatorStack.pop().symbol);
+			}
+
+			operatorStack.push(operator);
+		}
+	}
+
+	// add operators still in the stack to output queue
+	for(let operator of operatorStack) {
+		outputQueue.push(operator.symbol);
+	}
+
+	return outputQueue.join(' ');
+}
+
+module.exports = { convertInfixToPostfix };
--- test/test-shunting-yard.js
+++ test/test-shunting-yard.js
@@ -1,13 +1,45 @@
 import { expect } from 'chai';
-import { convertInfixToRPN } from '../src/js/shunting-yard';
+import { convertInfixToPostfix } from '../src/js/shunting-yard';
 
 describe('The Shunting-yard converter', () => {
 
-	it('should convert "+" operator', function() {
+	it('should convert "+" operator', () => {
 
 		const infix = '1 + 2';
-		let rpn = convertInfixToRPN(infix);
+		let postfix = convertInfixToPostfix(infix);
 
-		expect(rpn).to.equal('1 2 +');
-	})
+		expect(postfix).to.equal('1 2 +');
+	});
+
+	it('should convert "-" operator', () => {
+		
+		const infix = '2 - 1';
+		let postfix = convertInfixToPostfix(infix);
+
+		expect(postfix).to.equal('2 1 -');
+	});
+
+	it('should convert "/" operator', () => {
+		
+		const infix = '4 / 2';
+		let postfix = convertInfixToPostfix(infix);
+
+		expect(postfix).to.equal('4 2 /');
+	});
+
+	it('should convert "*" operator', () => {
+		
+		const infix = '1 * 2';
+		let postfix = convertInfixToPostfix(infix);
+
+		expect(postfix).to.equal('1 2 *');
+	});
+
+	it('should convert "^" operator', () => {
+
+		const infix = '2 ^ 3';
+		let postfix = convertInfixToPostfix(infix);
+
+		expect(postfix).to.equal('2 3 ^');
+	});
 })
