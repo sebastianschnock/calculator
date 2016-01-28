2a582fc0b8df9e5e052c54543105b6f3aa4b3fdd
sebastianschnock <sebastian.schnock@gmail.com>
2016-01-28 20:08:28 +0100

    Add test for calculator and first implementation

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,3 +1,15 @@
-(() => {
-	console.log('test');
-})()
+import convertInfixToPostfix from './shunting-yard';
+import evaluate from './evaluate-postfix';
+
+/**
+ * Calculates a mathematical expression in infix notation.
+ *
+ * @method     calculate
+ * @param      {String}  expression  an expression in infix notation (eg. '1 + 2 * 3')
+ * @return     {Number}  the calulated result
+ */
+function calculate(expression) {
+	return evaluate(convertInfixToPostfix(expression));
+}
+
+export default calculate;
---
+++ test/test-calculator.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import calculate from '../src/js/calculator';
+
+describe('The calculator', () => {
+
+	it('should support addition', () => {
+		let result = calculate('1 + 2');
+		expect(result).to.equal(3);
+	});
+
+	it('should support subtraction', () => {
+		let result = calculate('6 - 2');
+		expect(result).to.equal(4);
+	});
+
+	it('should support division', () => {
+		let result = calculate('14 / 2');
+		expect(result).to.equal(7);
+	});
+
+	it('should support multiplication', () => {
+		let result = calculate('2 * 8');
+		expect(result).to.equal(16);
+	});
+
+	it('should support exponentiation', () => {
+		let result = calculate('5 ^ 3');
+		expect(result).to.equal(125);
+	});
+
+})
