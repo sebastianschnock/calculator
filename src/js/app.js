0ecd19e4cae4789b173d0bbbd4d8930e55fd90bf
sebastianschnock <sebastian.schnock@gmail.com>
2016-01-30 15:17:47 +0100

    Add first simple gui

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+import calculate from './calculator';
+
+(() => {
+
+	let calcElem = document.querySelector('.calc');
+	let displayElem = calcElem.querySelector('.calc__display');
+	
+	// set up digit and operator buttons
+	for(let elem of calcElem.querySelectorAll('[data-calc-input]')) {
+		addInput(elem, elem.dataset.calcInput);
+	}
+
+	// set up calculate button
+	let calculateElem = calcElem.querySelector('.calc__main--calculate');
+	calculateElem.addEventListener('click', () => {
+		displayElem.textContent = calculate(displayElem.textContent);
+	});
+
+	// set up clear button
+	let clearElem = calcElem.querySelector('.calc__main--clear');
+	clearElem.addEventListener('click', () => {
+		displayElem.textContent = '';
+	});
+
+
+	function addInput(elem, input) {
+		elem.addEventListener('click', () => {
+			displayElem.textContent += ` ${input}`;
+		});
+	}
+
+})(document)
